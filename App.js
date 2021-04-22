db50b9bcfd0287a9a84acfeffc5906acc92ecbb2
Oliver Rumagit <s21810511@student.unklab.ac.id>
2021-04-22 11:13:38 +0700

    Exercise#6

---
+++ App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import RegistrationPage from './src/component/pages/RegistrationPage';
+import UserListPage from './src/component/pages/UserListPage';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const [page, setPage] = useState('Registration')
+
+  if (page == 'Registration') {
+    return (
+      <View>
+        <RegistrationPage changePageHandler={()=> setPage('UserList')}/>
+      </View>
+    )
+  } else if (page == 'UserList'){
+    return (
+      <View>
+        <UserListPage/>
+      </View>
+    )
+  }
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# /d/mobileprogramming/exercise6/MobPro_Exercises-06-main/MobPro_Exercises-06-main
